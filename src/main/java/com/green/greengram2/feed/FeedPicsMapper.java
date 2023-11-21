2303315bc3c1ec12898ac16d35669c41d8c12800
DoheumPark <pirbak@hanmail.net>
2023-11-21 16:56:31 +0900

    11-21 : 7교시 (feed 리스트)

--- src/main/java/com/green/greengram2/feed/FeedPicsMapper.java
+++ src/main/java/com/green/greengram2/feed/FeedPicsMapper.java
@@ -1,16 +1,13 @@
 package com.green.greengram2.feed;
 
 import com.green.greengram2.feed.model.FeedInsProcDto;
-import com.green.greengram2.feed.model.FeedSelDto;
 import com.green.greengram2.feed.model.FeedSelVo;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
-public interface FeedMapper {
-    int insFeed(FeedInsProcDto dto);
-
-
-    List<FeedSelVo> selFeedAll(FeedSelDto dto);
+public interface FeedPicsMapper {
+    int insFeedPics(FeedInsProcDto dto);
+    List<String> selFeedPicsAll(int ifeed);
 }
